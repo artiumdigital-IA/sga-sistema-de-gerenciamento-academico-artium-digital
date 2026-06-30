bef7f2634e486888899267c3386c9d7d557eb355
FIURJ TI <fiurjids@gmail.com>
2026-06-30 10:43:11 -0300

    fix: FinancialModule faltava importar AuditModule - causava crash no startup

--- backend/src/financial/financial.module.ts
+++ backend/src/financial/financial.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuditModule } from '../audit/audit.module';
 import { ContratoController } from './contrato/contrato.controller';
 import { ContratoService } from './contrato/contrato.service';
 import { ParcelaController } from './parcela/parcela.controller';
 import { ParcelaService } from './parcela/parcela.service';
 
 @Module({
+  imports: [AuditModule],
   controllers: [ContratoController, ParcelaController],
   providers: [ContratoService, ParcelaService],
 })
